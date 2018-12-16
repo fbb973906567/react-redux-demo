36843168c2c6fabac8dec8aa90c2b8aa5e706624
hpfan <973906567@qq.com>
2018-12-16 13:29:01 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import Button from 'antd/lib/button';
+
+import {connect} from 'react-redux';
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      num:0
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Button type="primary" onClick={this.props.Add}>+</Button>
+       <span className="numValue">{this.state.num}</span>
+       <Button type="primary" onClick={this.props.Del}>-</Button>
+      </div>
+    );
+  }
+  componentWillReceiveProps(state){
+      this.setState({
+        num:state.num
+      })
+  }
+}
+const mapStateToProps=function(state){
+  debugger
+  return state?state:{};
+}
+
+const mapDispatchToProps=function(dispatch){
+  return {
+      Add:()=>{dispatch({type:'INCREMENT'})},
+      Del:()=>{dispatch({type:"DECREMENT"})}
+  }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(App);
---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+const reducer=function(state={num:0},action){
+        switch(action.type){
+                case "INCREMENT":
+                return {...state,num:state.num+1}
+                case "DECREMENT":
+                return {...state,num:state.num-1}
+               default:
+               break
+                
+        }
+}
+
+export default reducer;
